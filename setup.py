22d5cebf12047a54ebffae0ffa455c2c91a73e62
SaschaSchweitzer <32032752+SaschaSchweitzer@users.noreply.github.com>
2017-09-17 19:45:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(name='persons',
-      version='0.1a',
+      version='0.2a',
       description='Graph-based author disambiguation. Identifies distinct persons by matching forenames and surnames. Supports known persons, year data, and others.',
       url='https://github.com/SaschaSchweitzer/persons',
       author='Sascha Schweitzer',
       author_email='sascha.schweitzer@gmail.com',
       license='Apache License, Version 2.0',
       packages=find_packages(),
-      long_description=open('README.md').read(),
       install_requires=[
           'pytz',
       ],
